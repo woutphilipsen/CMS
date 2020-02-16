e8c18ece9b005fb91c10f2e2c2422dc8c1cbaad4
philipsenwout <philipsenwout@outlook.com>
2020-02-16 18:47:22 +0100

    2. email object setup with mailerInterface

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,6 +8,8 @@ use App\Form\UserRegistrationFormType;
 use App\Security\LoginFormAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
@@ -43,7 +45,7 @@ class SecurityController extends AbstractController
     /**
      * @Route("/register", name="app_register")
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $formAuthenticator)
+    public function register(MailerInterface $mailer,Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $formAuthenticator)
     {
         $form = $this->createForm(UserRegistrationFormType::class);
         $form->handleRequest($request);
@@ -68,6 +70,16 @@ class SecurityController extends AbstractController
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
+
+            // --------------- EMAIL SETUP --------------------------- //
+            $email = (new Email())
+                ->from('alienmailr@example.com')
+                ->to($user->getEmail())
+                ->subject('Welcome to the Space Bar!')
+                ->text("Nice to meet you {$user->getFirstName()}! ");
+
+            $mailer->send($email);
+            // ------------------------------------------------------- //
 
             return $guardHandler->authenticateUserAndHandleSuccess(
                 $user,
