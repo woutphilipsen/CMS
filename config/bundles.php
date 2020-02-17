150f605acb0ba51e381de39a52591c4e91cc8297
philipsenwout <philipsenwout@outlook.com>
2020-02-17 14:41:38 +0100

    15. snappy wkhtmltopdf

--- config/bundles.php
+++ config/bundles.php
@@ -24,4 +24,5 @@ return [
     Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
 ];
